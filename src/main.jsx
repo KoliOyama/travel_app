46ebc6f0f4a25648408ba3ec359c5ffb4ad4a871
Koli Oyama <104010042+KoliOyama@users.noreply.github.com>
2025-07-28 11:05:25 +0100

    fix: update router and Vite config for deployment compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <Router basename="/travel_app">
+  <Router>
     <App />
   </Router>
   </StrictMode>,
